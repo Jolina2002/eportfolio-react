8fa9046000c7b2787efb9895905fcfd82f0899d4
jolinaheesen <jolina.heesen@gmail.com>
2023-05-30 18:11:20 +0200

    Set up project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <MyButton/>
+          <ButtonCounter/>
+          <ToggleComponent/>
+          <TodoList/>
       </header>
     </div>
   );
 }
+
+// DEMO CODE:
+function MyButton() {
+    return (
+        <button>I'm a button</button>
+    );
+}
+// END OF DEMO CODE
+
+// EXERCISE 1:
+//Create a React component that displays a button and a number. Each time the button is clicked, the number should increase by one.
+
+
+// EXERCISE 2:
+//Create a toggle component that contains a button. When the button is clicked, the text should toggle between "On" and "Off".
+
+
+// EXERCISE 3:
+//Create a simple to-do list. The component should contain a prompt and a button to add an item to the list. The list of items should be displayed below.
+
 
 export default App;
