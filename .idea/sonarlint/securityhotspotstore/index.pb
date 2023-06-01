535a3615c5750f8ac51d90daed9f8ef0e6e30d8b
jolinaheesen <jolina.heesen@gmail.com>
2023-06-01 09:33:19 +0200

    added solutions for exercises

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+?
+src/App.test.js,9/2/923703ced91ac38943d37930d9d27a7ebe8f01ad
+=
+src/index.css,c/5/c5d38a62d1faae90a2acaa34b79a7ef365c917e4
--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
           <MyButton/>
+          <ButtonCounter/>
+          <ToggleComponent/>
+          <TodoList/>
       </header>
     </div>
   );
@@ -21,14 +24,61 @@ function MyButton() {
 
 // EXERCISE 1:
 //Create a React component that displays a button and a number. Each time the button is clicked, the number should increase by one.
+function ButtonCounter() {
+    const [count, setCount] = useState(0);
+    const incrementCount = () => setCount(count + 1);
 
-
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={incrementCount}>Click me</button>
+        </div>
+    );
+}
 // EXERCISE 2:
 //Create a toggle component that contains a button. When the button is clicked, the text should toggle between "On" and "Off".
+function ToggleComponent() {
+    const [isOn, setIsOn] = useState(false);
+    const toggle = () => setIsOn(!isOn);
 
+    return (
+        <div>
+            <button onClick={toggle}>{isOn ? 'ON' : 'OFF'}</button>
+        </div>
+    );
+        }
 
 // EXERCISE 3:
 //Create a simple to-do list. The component should contain a prompt and a button to add an item to the list. The list of items should be displayed below.
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState('');
+
+
+    const addTodo = () => {
+        if (newTodo.trim() !== '') {
+            setTodos([...todos, newTodo]);
+            setNewTodo('');
+        }
+    };
+
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+            />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 
 export default App;
